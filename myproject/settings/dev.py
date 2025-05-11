25248a4393fb2257c97dedf094decaca2ff359b4
AlexQasper <semidog@gmail.com>
2025-05-11 22:49:24 +0400

    first commit

---
+++ myproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zyiup@@i2c8h@s#_wya0hp@wh==04oo6!&3#0y+r@25zx(y!2v"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
